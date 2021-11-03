8a561528d7158d09d55098271def1aab44580f52
IsratNourin <isratnourin5@gmail.com>
2021-11-03 18:16:26 +0600

    fixed List group

--- index.js
+++ index.js
@@ -20,3 +20,13 @@ $(document).ready(function(){
         }]
     });
 });
+
+var triggerTabList = [].slice.call(document.querySelectorAll('#list-tab a'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
